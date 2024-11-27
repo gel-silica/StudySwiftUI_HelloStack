39fd171d3b972e25d1a1a5ec9d24115cb927487b
silicagel <ksk19981004@gmail.com>
2024-11-28 03:34:31 +0900

    Initial Commit

---
+++ HelloStack/HelloStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloStackApp.swift
+//  HelloStack
+//
+//  Created by silicagel on 2024/11/28.
+//
+
+import SwiftUI
+
+@main
+struct HelloStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
