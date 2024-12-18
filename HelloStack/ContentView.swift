b779d6be686d97d4bbb43a4d5d3e8e82143ba3d5
silicagel <ksk19981004@gmail.com>
2024-12-18 21:54:11 +0900

    test

--- HelloStack/ContentView.swift
+++ HelloStack/ContentView.swift
@@ -10,15 +10,78 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            //頭部
+            ZStack {
+                //顔面
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 300, height: 300)
+                VStack {
+                    //ゴーグル
+                    HStack {
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 60, height: 20)
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.gray)
+                                .frame(width: 90, height: 90)
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .frame(width: 70, height: 70)
+                            Rectangle()
+                                .foregroundColor(.black)
+                                .frame(width: 20, height: 20)
+                        }
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.gray)
+                                .frame(width: 90, height: 90)
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .frame(width: 70, height: 70)
+                            Rectangle()
+                                .foregroundColor(.black)
+                                .frame(width: 20, height: 20)
+                        }
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 60, height: 20)
+                    }
+                    //クチ
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 70, height: 40)
+                        VStack {
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .frame(width: 60, height: 10)
+                            Rectangle()
+                                .foregroundColor(.red)
+                                .frame(width: 60, height: 10)
+                        }
+                    }
+                }
+            }
+            //胴体
+            Rectangle()
+                .foregroundColor(.blue)
+                .frame(width: 300, height: 100)
+            //脚
+            HStack {
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(width: 70, height: 30)
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(width: 70, height: 30)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
